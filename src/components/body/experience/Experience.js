10409e7299683919cd959ab951bf1a6964297de6
Ethan-Shapiro <46407744+Ethan-Shapiro@users.noreply.github.com>
2023-03-17 16:25:06 -0700

    added project cards

--- src/components/body/experience/Experience.js
+++ src/components/body/experience/Experience.js
@@ -5,6 +5,7 @@ import Tab from "@mui/material/Tab";
 import JobCard from "./JobCard/JobCard";
 import { JobData } from "../../data/JobData";
 import PropTypes from "prop-types";
+import styled from "styled-components";
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -17,7 +18,9 @@ function TabPanel(props) {
       aria-labelledby={`vertical-tab-${index}`}
       {...other}
     >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+      {value === index && (
+        <Box sx={{ p: 3, pt: 0, maxWidth: 600 }}>{children}</Box>
+      )}
     </div>
   );
 }
@@ -45,33 +48,38 @@ const Experience = () => {
   };
 
   return (
-    <div style={{ textAlign: "start" }}>
-      <Typography variant="h3">{"Experience"}</Typography>
-      <Box
-        sx={{
-          flexGrow: 1,
-          bgcolor: "background.paper",
-          display: "flex",
-          height: "100%",
-        }}
-      >
-        <Tabs
-          orientation="vertical"
-          variant="scrollable"
-          value={currTab}
-          onChange={handleChange}
-          aria-label="Vertical tabs example"
-          sx={{ borderRight: 1, borderColor: "divider", minWidth: 130 }}
+    <div
+      style={{ textAlign: "start", display: "flex", justifyContent: "center" }}
+    >
+      <Box sx={{ width: 850 }}>
+        <Typography variant="h4">{"Experience"}</Typography>
+        <Box
+          sx={{
+            flexGrow: 1,
+            bgcolor: "background.paper",
+            display: "flex",
+            minHeight: 370,
+            height: "100%",
+          }}
         >
+          <Tabs
+            orientation="vertical"
+            variant="scrollable"
+            value={currTab}
+            onChange={handleChange}
+            aria-label="Vertical tabs example"
+            sx={{ borderRight: 1, borderColor: "divider", width: 140 }}
+          >
+            {jobs.map((job, index) => (
+              <Tab label={job["company"]} {...a11yProps(index)} />
+            ))}
+          </Tabs>
           {jobs.map((job, index) => (
-            <Tab label={job["company"]} {...a11yProps(index)} />
+            <TabPanel value={currTab} index={index}>
+              <JobCard key={job["id"]} {...job}></JobCard>
+            </TabPanel>
           ))}
-        </Tabs>
-        {jobs.map((job, index) => (
-          <TabPanel value={currTab} index={index}>
-            <JobCard key={job["id"]} {...job}></JobCard>
-          </TabPanel>
-        ))}
+        </Box>
       </Box>
     </div>
   );
